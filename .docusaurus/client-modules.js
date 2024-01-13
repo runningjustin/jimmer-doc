4f372c6246f03e3eb72766830c5e2d3211f49739
babyfish-ct <babyfish.ct@gmail.com>
2024-01-13 22:12:50 +0800

    gh-pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/chentao/projects/git/jimmer-doc/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/chentao/projects/git/jimmer-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/chentao/projects/git/jimmer-doc/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/chentao/projects/git/jimmer-doc/src/css/custom.css'),
+];
